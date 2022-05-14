fbe64e2e46729a74f636005e819f719f525c9160
Ксения Сажнева <13pokemon@mail.ru>
2022-05-14 23:15:05 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2180;
+        int bonus = amount / 20;
+
+        System.out.println("Сумма покупки: " + amount + " руб.");
+        System.out.println("Ваш бонус: " + bonus + " миль");
+    }
+}
